58c156c11a046a45bf9077e302a29f68057d2a9e
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-11-03 22:49:59 +0200

    добавлен шлях для логінації користувача

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("registration/", views.registration, name="registration"),
     path("profile/", views.profile, name="profile"),
     path("update/", views.update_profile_and_user, name="update"),
     path("logout/", views.custom_logout, name="custom_logout"),
-    path("change_password/", views.change_password, name="change_password")
+    path("change_password/", views.change_password, name="change_password"),
+    path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login")
 ]
