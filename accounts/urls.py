418c9bebf2877f55dc553d86be365084003fcea2
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-11-04 22:48:58 +0200

    створюємо доступ до файлів /media та додані коментарі

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("registration/", views.registration, name="registration"),
@@ -10,3 +12,25 @@ urlpatterns = [
     path("change_password/", views.change_password, name="change_password"),
     path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login")
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+    # if settings.DEBUG:
+    # -------------------
+    # на продакшені (DEBUG=False) ця стрічка не працює,
+    # тому медіа й статичні файли тоді обслуговуються через вебсервер (наприклад, Render).
+    # --------------------------------------------------------------------------------------
+
+    # urlpatterns += ...
+    # -------------------
+    #  до існуючого списку urlpatterns (де прописані всі твої path() і include())
+    #  додаються нові маршрути (URL) для обслуговування файлів.
+    # ---------------------------------------------------------------------------
+
+    # static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # -------------------------------------------------------------------------
+    # створює “тестовий” маршрут, який дозволяє Django показувати медіафайли
+    # (наприклад, аватарки, картинки, завантаження) напряму під час розробки.
