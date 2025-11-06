078f81df8eb772c5a68d59f368dbe607395ace0b
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-11-06 17:11:22 +0200

    змінен тимчасовий основний шлях з аккаунтів на фільми

--- movie_library/urls.py
+++ movie_library/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include(("accounts.urls", "accounts"), namespace="accounts")),
+    path("accounts/", include(("accounts.urls", "accounts"), namespace="accounts")),
+    path("", include(("movies.urls", "movies"), namespace="movies")),
 ]
