56a850fbc46f4d114882dd4680dd3aa6c68cc45c
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-11-09 19:09:35 +0200

    створена форма MovieForm на основі моделі Movie

---
+++ movies/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from .models import Movie
+
+class MovieForm(forms.ModelForm):
+    class Meta:
+        model = Movie
+        fields = [
+            "title",
+            "poster",
+            "description",
+            "rating",
+            "genre",
+            "duration",
+            "actors",
+            "release_date",
+            "status"
+        ]
+        widgets = {
+            "title" : forms.TextInput(attrs={
+                "class" : "form-control",
+                "placeholder" : "Enter movie title",
+            }),
+            # ClearableFileInput -- дає можливість змінювати або очищати завантажений файл
+            "poster" : forms.ClearableFileInput(attrs={
+                "class" : "form-control",
+            }),
+            "description" : forms.Textarea(attrs={
+                "class" : "form-control",
+                "rows" : 4,
+                "placeholder" : "Write a description...",
+            }),
+            "rating" : forms.NumberInput(attrs={
+                "class" : "form-control",
+                "min" : 0,
+                "max" : 10,
+            }),
+            "genre" : forms.Select(attrs={
+                "class" : "form-control",
+            }),
+            "duration" : forms.NumberInput(attrs={
+                "class" : "form-control",
+            }),
+            "actors" : forms.TextInput(attrs={
+                "class" : "form-control",
+                "placeholder" : "List main actors",
+            }),
+            "release_date" : forms.DateInput(attrs={
+                "class" : "form-control",
+                "type" : "date",
+            }),
+        }
