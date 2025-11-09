ffb30893ebdee9ba85d4f88431995cf53e19d89f
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-11-09 19:23:32 +0200

    створюємо доступ до файлів /media

--- movies/urls.py
+++ movies/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import create_movie, movie_list, movie_detail, movie_update, movie_delete
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", movie_list, name="movie_list"),
@@ -8,3 +10,6 @@ urlpatterns = [
     path("movie_update/<int:id>/", movie_update, name="movie_update"),
     path("movie_delete/<int:id>/", movie_delete, name="movie_delete"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
